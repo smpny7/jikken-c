2906bf589aa7d233435f344ec21602e30560ba84
smpny7 <symphonymode7@icloud.com>
2022-01-11 09:23:03 +0000

    Tmp: 構文解析 EOF到達

--- assembly/final/2/ast.c
+++ assembly/final/2/ast.c
@@ -3,13 +3,29 @@
 #include <string.h>
 #include "ast.h"
 
+extern int yylex();
+extern int yyerror();
+extern int yyparse();
+
 Node *build_node2(NodeType nType, Node *np1, Node *np2)
+{
+    Node *np; // TODO 一行可能？
+    np = (Node *)malloc(sizeof(Node));
+    np->nType = nType;
+    np->child = (struct Node *)np1;
+    np1->brother = (struct Node *)np2;
+    np->brother = NULL;
+    return np;
+}
+
+Node *build_node3(NodeType nType, Node *np1, Node *np2, Node *np3)
 {
     Node *np;
     np = (Node *)malloc(sizeof(Node));
     np->nType = nType;
     np->child = (struct Node *)np1;
     np1->brother = (struct Node *)np2;
+    np2->brother = (struct Node *)np3;
     np->brother = NULL;
     return np;
 }
@@ -52,6 +68,26 @@ Node *build_array_node(NodeType nType, char *varName, int value)
 
 void printTree(Node *np)
 {
+    /* ==  for debug  ================== */
+
+    char *node_type_str[] = {
+        "Pro_AST",    // プログラムのノード型
+        "Decls_AST",  // 変数宣言部のノード型
+        "Stats_AST",  // 文集合のノード型
+        "Stat_AST",   // 文のノード型
+        "Assign_AST", // 代入文のノード型
+        "Add_AST",    // 加減のノード型
+        "Mul_AST",    // 乗除のノード型
+        "Ident_AST",  // 識別子のノード型
+        "Number_AST", // 整数のノード型
+        "ArryEl_AST", // 配列のノード型
+        "While_AST",  // ループ文のノード型
+        "If_AST",     // 条件分岐文のノード型
+        "Cond_AST",   // 条件式のノード型
+    };
+
+    /* ================================= */
+
     printf("%s", node_type_str[np->nType]);
     if (np->varName && np->value)
         printf("[= %s[%d]]", np->varName, np->value);
@@ -60,30 +96,50 @@ void printTree(Node *np)
     else if (np->value)
         printf("[= %d]", np->value);
 
-    if (np->brother != NULL)
-    {
-        printf(" ");
-        printTree((Node *)np->brother);
-    }
     if (np->child != NULL)
     {
         printf("(");
         printTree((Node *)np->child);
         printf(")");
     }
+    if (np->brother != NULL)
+    {
+        printf(" ");
+        printTree((Node *)np->brother);
+    }
 }
+
+// int main(void)
+// {
+//     Node *top;
+//     Node *p1;
+//     Node *p2;
+//     p1 = build_node2(Add_AST,
+//                      build_array_node(ArryEl_AST, "a", 2),
+//                      build_num_node(Number_AST, 1));
+//     p2 = build_node3(Add_AST,
+//                      build_array_node(ArryEl_AST, "a", 20),
+//                      build_num_node(Number_AST, 10),
+//                      build_ident_node(Ident_AST, "x"));
+//     top = build_node3(Assign_AST,
+//                       build_ident_node(Ident_AST, "x"),
+//                       p1,
+//                       p2);
+//     printTree(top);
+//     printf("\n");
+//     return 0;
+// }
 
 int main(void)
 {
-    Node *top;
-    Node *p1;
-    p1 = build_node2(Add_AST,
-                     build_array_node(ArryEl_AST, "a", 20),
-                     build_num_node(Number_AST, 10));
-    top = build_node2(Assign_AST,
-                      build_ident_node(Ident_AST, "x"),
-                      p1);
+    if (yyparse())
+    {
+        fprintf(stderr, "Error\n");
+        return 1;
+    }
+
     printTree(top);
     printf("\n");
+
     return 0;
 }
--- assembly/final/2/ast.h
+++ assembly/final/2/ast.h
@@ -2,14 +2,19 @@
 
 typedef enum
 {
-    Pro_AST,    //プログラムのノード型
-    Stats_AST,  //文集合のノード型
+    Pro_AST,    // プログラムのノード型
+    Decls_AST,  // 変数宣言部のノード型
+    Stats_AST,  // 文集合のノード型
+    Stat_AST,   // 文のノード型
+    Assign_AST, // 代入文のノード型
+    Add_AST,    // 加減のノード型
+    Mul_AST,    // 乗除のノード型
     Ident_AST,  // 識別子のノード型
     Number_AST, // 整数のノード型
     ArryEl_AST, // 配列のノード型
-
-    Add_AST,    // 和のノード型
-    Assign_AST, // 代入文の=のノード型
+    While_AST,  // ループ文のノード型
+    If_AST,     // 条件分岐文のノード型
+    Cond_AST,   // 条件式のノード型
 } NodeType;
 
 typedef struct abstract_node
@@ -21,22 +26,11 @@ typedef struct abstract_node
     struct Node *brother;
 } Node;
 
+Node *top;
+
+// TODO: いらない？
 Node *build_node2(NodeType nType, Node *np1, Node *np2);
+Node *build_node3(NodeType nType, Node *np1, Node *np2, Node *np3);
 Node *build_ident_node(NodeType nType, char *varName);
 Node *build_num_node(NodeType nType, int value);
 Node *build_array_node(NodeType nType, char *varName, int value);
-
-/* ==  for debug  ================== */
-
-char *node_type_str[] = {
-    "Pro_AST",    //プログラムのノード型
-    "Stats_AST",  //文集合のノード型
-    "Ident_AST",  // 識別子のノード型
-    "Number_AST", // 整数のノード型
-    "ArryEl_AST", // 配列のノード型
-
-    "Add_AST",    // 和のノード型
-    "Assign_AST", // 代入文の=のノード型
-};
-
-/* ================================= */
